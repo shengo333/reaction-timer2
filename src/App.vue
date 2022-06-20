070a74d0c8fbc692f143627034ca4b87b33e135c
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-06-20 14:45:11 +0400

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay = "delay"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue'
 
 export default {
+
+
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start(){
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      console.log(this.delay, 'this is delay')
+    }
   }
 }
 </script>
@@ -20,7 +37,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="block" v-if="showBlock">
+    click me
+  </div>
+</template>
+
+<script>
+import { onUnmounted, onUpdated } from '@vue/runtime-core'
+
+
+export default {
+    props: ['delay'],
+    data(){
+      return{
+        showBlock: false
+      }
+    },
+    mounted(){
+      setTimeout(()=>{
+        this.showBlock = true
+      }, this.delay )
+    },
+    updated(){
+      console.log('component updated')
+    },
+    unmounted(){
+      console.log('unmounted')
+    }
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>

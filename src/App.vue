df6555b583a2aa2fd58c449af02ddb264d58ba58
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-06-21 10:42:06 +0400

    finished

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,27 @@
   <div>
     <h1>Reaction Timer</h1>
     <button @click="start" :disabled="isPlaying">Play</button>
-    <Block v-if="isPlaying" :delay = "delay"/>
+    <Block v-if="isPlaying" :delay = "delay" @end="endGame"/>
+    <Results v-if="showResults" :score="score"/>
   </div>
 </template>
 
 <script>
 
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
 
 
   name: 'App',
-  components: { Block},
+  components: { Block, Results},
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
@@ -26,6 +30,12 @@ export default {
       this.isPlaying = true;
       this.delay = 2000 + Math.random() * 5000;
       console.log(this.delay, 'this is delay')
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
@@ -39,5 +49,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 1rem;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
